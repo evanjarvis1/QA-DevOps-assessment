578548c8932756512a5a90e7d1030c98ff4f53a0
Evan <evanlegee@gmail.com>
2023-04-14 15:32:44 -0600

    added some stuff

--- __tests__/shuffle.test.js
+++ __tests__/shuffle.test.js
@@ -2,4 +2,11 @@ const shuffle = require("../src/shuffle");
 
 describe("shuffle should...", () => {
   // CODE HERE
+
+  const copyArray = require("src\shuffle.js")
+
+test("")
+expect(copyArray().not.toBeUndefined())
+
 });
+
